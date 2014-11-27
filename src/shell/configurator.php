2228fd09800344ae696181f2c4cd68f39febe5ae
Johannes Klein <jo@temka.de>
2014-11-27 15:13:31 +0100

    Update configurator.php
    fixes #1

--- src/shell/configurator.php
+++ src/shell/configurator.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once 'abstract.php';
+if (file_exists('shell/abstract.php')) {
+    require_once 'shell/abstract.php';
+}
+else {
+    exit('Please call the Magento configurator from the Magento root dir.');
+}
 
 class Cti_Configurator_Shell extends Mage_Shell_Abstract {
 
