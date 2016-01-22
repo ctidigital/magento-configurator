454da65f84fe6d56e597221e4ddce0403b1d8e11
Devesh Chevli <d.chevli@ctidigital.com>
2016-01-22 17:08:44 +0000

    SQL sample files addition

---
+++ samples/components/sql/sitemap.sql
@@ -0,0 +1,6 @@
+LOCK TABLES `sitemap` WRITE;
+TRUNCATE TABLE `sitemap`;
+INSERT INTO `sitemap` (`sitemap_id`, `sitemap_type`, `sitemap_filename`, `sitemap_path`, `sitemap_time`, `store_id`)
+VALUES
+	(1, NULL, 'sitemap.xml', '/', '2015-08-21 15:13:24', 1);
+UNLOCK TABLES;
