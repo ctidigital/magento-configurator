07993713f9b4da99130f399c5acc2aaeedcb89de
Devesh Chevli <d.chevli@ctidigital.com>
2016-02-01 15:08:41 +0000

    Change test stand 5

---
+++ src/app/code/community/Cti/Configurator/Test/Helper/Website.php
@@ -0,0 +1,12 @@
+<?php
+class Cti_Configurator_Test_Helper_Website extends EcomDev_PHPUnit_Test_Case {
+
+    /**
+     * Example test
+     *
+     * @test
+     */
+    public function test() {
+        $this->assetEquals(1,1);
+    }
+}
